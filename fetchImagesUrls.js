8398aba79a9fdc62dec398310d9ea20adade994a
Kathy Nguyen <kathyn262@gmail.com>
2020-04-17 16:49:48 -0700

    removed unnecessary lines

--- fetchImagesUrls.js
+++ fetchImagesUrls.js
@@ -4,12 +4,10 @@ const fs = require('fs');
 let content = ''
 
 
-
 axios.get('https://www.ign.com/wikis/animal-crossing-new-horizons/Villagers_and_Other_Characters').then(function(resp) {
   let pattern = /https:\/\/oyster.+?\/animal-crossing-new-horizons\/.+?\.(?:jpg|gif|png)/g;
   content = resp.data.match(pattern);
   let unique = new Set(content);
-  console.log(unique)
 
   fs.writeFile('./output.txt', util.inspect(unique), "utf8", function(err) {
     if (err) {
