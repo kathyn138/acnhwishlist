6a8f7f923fd2fa7afdb7984ab0a21e7ec362d372
Kathy Nguyen <kathyn262@gmail.com>
2020-04-30 14:55:26 -0700

    added villager model

---
+++ backend/models/villager.js
@@ -0,0 +1,125 @@
+const db = require("../db");
+
+/** Related functions for villagers. */
+
+class Villager {
+
+  /** Find all Cranky villagers. */
+
+  static async findAllCranky() {
+    const crankyRes = await db.query(
+      `SELECT id, name, image, personality
+          FROM villagers
+          WHERE personality = 'Cranky'
+          ORDER BY name`);
+
+    return crankyRes.rows;
+  }
+
+  /** Find all Jock villagers. */
+
+  static async findAllJock() {
+    const jockRes = await db.query(
+      `SELECT id, name, image, personality
+          FROM villagers
+          WHERE personality = 'Jock'
+          ORDER BY name`);
+
+    return jockRes.rows;
+  }
+
+  /** Find all Lazy villagers. */
+
+  static async findAllLazy() {
+    const lazyRes = await db.query(
+      `SELECT id, name, image, personality
+          FROM villagers
+          WHERE personality = 'Lazy'
+          ORDER BY name`);
+
+    return lazyRes.rows;
+  }
+
+  /** Find all Normal villagers. */
+
+  static async findAllNormal() {
+    const normalRes = await db.query(
+      `SELECT id, name, image, personality
+          FROM villagers
+          WHERE personality = 'Normal'
+          ORDER BY name`);
+
+    return normalRes.rows;
+  }
+
+  /** Find all Peppy villagers. */
+
+  static async findAllPeppy() {
+    const peppyRes = await db.query(
+      `SELECT id, name, image, personality
+          FROM villagers
+          WHERE personality = 'Peppy'
+          ORDER BY name`);
+
+    return peppyRes.rows;
+  }
+
+  /** Find all Smug villagers. */
+
+  static async findAllSmug() {
+    const smugRes = await db.query(
+      `SELECT id, name, image, personality
+          FROM villagers
+          WHERE personality = 'Smug'
+          ORDER BY name`);
+
+    return smugRes.rows;
+  }
+
+  /** Find all Snooty villagers. */
+
+  static async findAllSnooty() {
+    const snootyRes = await db.query(
+      `SELECT id, name, image, personality
+          FROM villagers
+          WHERE personality = 'Snooty'
+          ORDER BY name`);
+
+    return snootyRes.rows;
+  }
+
+  /** Find all Uchi villagers. */
+
+  static async findAllUchi() {
+    const uchiRes = await db.query(
+      `SELECT id, name, image, personality
+          FROM villagers
+          WHERE personality = 'Uchi'
+          ORDER BY name`);
+
+    return uchiRes.rows;
+  }
+
+  /** Given a villager name, return data about villager. */
+
+  static async findOne(name) {
+    const villagerRes = await db.query(
+      `SELECT id, name, image, personality
+            FROM villagers
+            WHERE name = $1`,
+      [name]);
+
+    const villager = villagerRes.rows[0];
+
+    if (!villager) {
+      const error = new Error(`There exists no villager '${name}'`);
+      error.status = 404;   // 404 NOT FOUND
+      throw error;
+    }
+
+    return villager;
+  }
+}
+
+
+module.exports = Villager;
