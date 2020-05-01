d93e9bb3c8767fc34cc69ddb0712a8a26e202ff4
Kathy Nguyen <kathyn262@gmail.com>
2020-04-30 17:43:34 -0700

    added search function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,43 @@ import './App.css';
 import SearchBar from './SearchBar';
 import VillagerList from './VillagerList';
 import Wishlist from './Wishlist';
+import wishlistApi from './wishlistApi';
 
-class App extends React.PureComponent {
+type appState = {
+  villagers: {
+    id: string, 
+    name: string, 
+    image: string, 
+    personality: string
+  }[], 
+  wishlist: {
+    id: string, 
+    name: string, 
+    image: string, 
+    personality: string
+  }[];
+}
+class App extends React.PureComponent<{}, appState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      villagers: [],
+      wishlist: []
+    };
+    this.searchVillagers = this.searchVillagers.bind(this);
+  }
+
+  async searchVillagers(query: string) {
+    let result = await wishlistApi.searchVillagers(query);
+    this.setState({ villagers: result });
+  }
 
   render() {
     return (
       <div className="App container-fluid">
         <div className="row">
           <div className="middle-section col-7">
-            <SearchBar />
+            <SearchBar search={this.searchVillagers} />
             <VillagerList />
           </div>
           <div className="col-5">
--- frontend/src/SearchBar.tsx
+++ frontend/src/SearchBar.tsx
@@ -1,13 +1,41 @@
 import React from 'react';
 import './SearchBar.css';
+import wishlistApi from './wishlistApi';
 
-class SearchBar extends React.PureComponent {
+type SearchBarProps = {
+  search: (query: string) => void;
+};
+
+type SearchBarState = {
+  query: string;
+};
+
+class SearchBar extends React.PureComponent<SearchBarProps, SearchBarState> {
+  constructor(props: SearchBarProps) {
+    super(props);
+    this.state = {
+      query: ''
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(evt: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({
+      [evt.target.name]: evt.target.value
+    } as SearchBarState);
+  }
+
+  handleSubmit(evt: React.FormEvent<HTMLFormElement>) {
+    evt.preventDefault();
+    this.props.search(this.state.query);
+  }
 
   render() {
     return (
-      <form className="form-inline my-5">
-        <input className="form-control col-7" id="search-input" type="search" placeholder="Search"
-          aria-label="Search" name="search" 
+      <form className="form-inline my-5" onSubmit={this.handleSubmit}>
+        <input className="form-control col-7" id="search-input" type="search" placeholder="Search for your favorite villager!"
+          aria-label="Search" name="query" value={this.state.query} onChange={this.handleChange}
          />
       </form>
     );
