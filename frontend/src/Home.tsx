e0de30b2a411a02a03250266d3123a4e8f8b7dbe
Kathy Nguyen <kathyn262@gmail.com>
2020-07-13 18:59:10 -0700

    set up villagerlist for loading screen

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -3,6 +3,7 @@ import './Home.css';
 import SearchBar from './SearchBar';
 import VillagerCard from './VillagerCard';
 import wishlistApi from './wishlistApi';
+import VillagerList from './VillagerList';
 
 type HomeProps = {
   villagers: {
@@ -32,32 +33,19 @@ type HomeProps = {
   checkWishlist: (name: string) => boolean;
 }
 
-// type HomeState = {
-//   villagers: {
-//     id: string,
-//     name: string,
-//     image: string,
-//     personality: string
-//   }[],
-// }
-
 class Home extends React.PureComponent<HomeProps> {
   constructor(props: HomeProps) {
     super(props);
-    // this.state = {
-    //   villagers: []
-    // };
     this.searchVillagers = this.searchVillagers.bind(this);
   }
 
   async searchVillagers(query: string) {
     let result = await wishlistApi.searchVillagers(query);
     this.props.updateVillagers(result);
-    console.log('in home', result)
   }
 
   render() {
-    let welcomePage = <React.Fragment>
+    let welcomeMessage = <React.Fragment>
       <img className="welcome-isabelle" src="https://cdn.discordapp.com/attachments/709246246359138316/709246283617009724/welcome-isabelle.png" alt=""></img>
       <p className="home-message">
         Search for your favorite villager or
@@ -65,23 +53,19 @@ class Home extends React.PureComponent<HomeProps> {
       </p>
     </React.Fragment>;
 
-    let searchList = this.props.villagers.map(v =>
-      <VillagerCard villager={v} addToWishlist={this.props.addToWishlist}
-        checkWishlist={this.props.checkWishlist}
-        removeFromWishlist={this.props.removeFromWishlist}
-      />);
-
-    let data = this.props.villagers.length ? searchList : welcomePage;
-
+    let homeData = this.props.villagers.length ? <VillagerList villagers={this.props.villagers}
+      updateVillagers={this.props.updateVillagers}
+      addToWishlist={this.props.addToWishlist}
+      checkWishlist={this.props.checkWishlist}
+      removeFromWishlist={this.props.removeFromWishlist} /> : welcomeMessage;
 
     return (
       <div className="middle-section col-md-7 text-center">
         <SearchBar search={this.searchVillagers} />
         <div className="home">
           <div className="home-data">
-            {data}
+            {homeData}
           </div>
-          {/* <div className="home-bot"></div> */}
         </div>
       </div>
     );
--- frontend/src/VillagerList.tsx
+++ frontend/src/VillagerList.tsx
@@ -8,7 +8,7 @@ import { RouteComponentProps } from 'react-router';
 interface MatchParams {
   personality: string;
 }
-interface VillagerListProps extends RouteComponentProps<MatchParams> {
+type VillagerListProps = {
   villagers: {
     id: string,
     name: string,
@@ -46,56 +46,66 @@ type VillagerListState = {
   searching: boolean,
   personality: string
 }
-class VillagerList extends React.PureComponent<VillagerListProps, VillagerListState> {
+class VillagerList extends React.PureComponent<VillagerListProps> {
   constructor(props: VillagerListProps) {
     super(props);
-    this.state = {
-      // villagers: [],
-      searching: false,
-      personality: this.props.match.params.personality
-    };
-    this.searchVillagers = this.searchVillagers.bind(this);
+    // this.state = {
+    //   // villagers: [],
+    //   searching: false,
+    //   personality: this.props.match.params.personality
+    // };
+    // this.searchVillagers = this.searchVillagers.bind(this);
   }
 
   // difference between update and mount here in logic? 
 
-  async componentDidUpdate(prevProps: VillagerListProps) {
-    if (this.props.match.params.personality !== prevProps.match.params.personality) {
-      let result = await wishlistApi.getVillagers(this.props.match.params.personality);
-      this.props.updateVillagers(result);
-      // this.setState({ villagers: result });
-    }
-  }
-  async componentDidMount() {
-    let result = await wishlistApi.getVillagers(this.props.match.params.personality);
-    this.props.updateVillagers(result);
-    // this.setState({ villagers: result });
-  }
+  // async componentDidUpdate(prevProps: VillagerListProps) {
+  //   if (this.props.match.params.personality !== prevProps.match.params.personality) {
+  //     let result = await wishlistApi.getVillagers(this.props.match.params.personality);
+  //     this.props.updateVillagers(result);
+  //     // this.setState({ villagers: result });
+  //   }
+  // }
+  // async componentDidMount() {
+  //   let result = await wishlistApi.getVillagers(this.props.match.params.personality);
+  //   this.props.updateVillagers(result);
+  //   // this.setState({ villagers: result });
+  // }
 
-  async searchVillagers(query: string) {
-    let result = await wishlistApi.searchVillagers(query);
-    this.props.updateVillagers(result);
-    this.setState({ searching: true });
-  }
+  // async searchVillagers(query: string) {
+  //   let result = await wishlistApi.searchVillagers(query);
+  //   this.props.updateVillagers(result);
+  //   this.setState({ searching: true });
+  // }
 
   render() {
-    let villagers = this.props.villagers ? this.props.villagers.map(v =>
-      <VillagerCard key={v.id} villager={v} addToWishlist={this.props.addToWishlist}
-        removeFromWishlist={this.props.removeFromWishlist}
-        checkWishlist={this.props.checkWishlist} />) : "Can't find that villager :( Try another one?";
+    // let villagers = this.props.villagers ? this.props.villagers.map(v =>
+    //   <VillagerCard key={v.id} villager={v} addToWishlist={this.props.addToWishlist}
+    //     removeFromWishlist={this.props.removeFromWishlist}
+    //     checkWishlist={this.props.checkWishlist} />) : "Can't find that villager :( Try another one?";
 
-    let header = this.state.searching ? "" : <h3>{this.props.match.params.personality} Villagers</h3>;
+    // let header = this.state.searching ? "" : <h3>{this.props.match.params.personality} Villagers</h3>;
+
+    let villagers = this.props.villagers.map(v =>
+      <VillagerCard villager={v} addToWishlist={this.props.addToWishlist}
+        checkWishlist={this.props.checkWishlist}
+        removeFromWishlist={this.props.removeFromWishlist}
+      />);
+
+    let loadingMessage = <React.Fragment>
+      <img className="loading-gif"
+        src="https://cdn.discordapp.com/attachments/709643259789705317/732414740134887545/tenor.gif"></img>
+      <p className="loading-message">
+        Loading...
+      </p>
+    </React.Fragment>
+
+    let villagerListData = this.props.villagers.length ? villagers : loadingMessage
 
     return (
-      <div className="middle-section col-md-7">
-        <SearchBar search={this.searchVillagers} />
-        <div className="vl">
-          <div className="vl-data">
-            <div className="vl-header">
-              {header}
-            </div>
-            {villagers}
-          </div>
+      <div className="vl">
+        <div className="vl-data">
+          {villagerListData}
         </div>
       </div>
     );
