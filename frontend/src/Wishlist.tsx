a856e767624fcb321cdb9bfd538e06b4d070fe30
Kathy Nguyen <kathyn262@gmail.com>
2020-07-19 05:15:03 -0700

    linked wishlist to / route

--- frontend/src/Wishlist.tsx
+++ frontend/src/Wishlist.tsx
@@ -22,17 +22,19 @@ class Wishlist extends React.PureComponent<WishlistProps> {
     this.props.wishlist.map(v => wishlistNames.push(v.name));
 
     return (
-      <div className="wl text-center">
-        <div className="wl-data">
-          <div className="wl-header">
-            <h3>Wishlist</h3>
+      <div className="col-md-5">
+        <div className="wl text-center">
+          <div className="wl-data">
+            <div className="wl-header">
+              <h3>Wishlist</h3>
+            </div>
+            {this.props.wishlist.map(v =>
+              <WishlistCard villager={v} removeFromWishlist={this.props.removeFromWishlist} />)}
           </div>
-          {this.props.wishlist.map(v =>
-            <WishlistCard villager={v} removeFromWishlist={this.props.removeFromWishlist} />)}
-        </div>
           <button type="button" className="btn wishlist-copy-btn"
             onClick={() => { navigator.clipboard.writeText(wishlistNames.join(', ')) }}>
             Copy to Clipboard</button>
+        </div>
       </div>
     );
   }
