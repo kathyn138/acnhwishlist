e076d0c85b85f36f4ec758096393d335e21caa45
Kathy Nguyen <kathyn262@gmail.com>
2020-07-18 22:35:55 -0700

    removed unused personality fns

--- backend/models/villager.js
+++ backend/models/villager.js
@@ -3,103 +3,6 @@ const db = require("../db");
 /** Related functions for villagers. */
 
 class Villager {
-
-  /** Find all Cranky villagers. */
-
-  static async findAllCranky() {
-    const crankyRes = await db.query(
-      `SELECT id, name, image, personality
-          FROM villagers
-          WHERE personality = 'Cranky'
-          ORDER BY name`);
-
-    return crankyRes.rows;
-  }
-
-  /** Find all Jock villagers. */
-
-  static async findAllJock() {
-    const jockRes = await db.query(
-      `SELECT id, name, image, personality
-          FROM villagers
-          WHERE personality = 'Jock'
-          ORDER BY name`);
-
-    return jockRes.rows;
-  }
-
-  /** Find all Lazy villagers. */
-
-  static async findAllLazy() {
-    const lazyRes = await db.query(
-      `SELECT id, name, image, personality
-          FROM villagers
-          WHERE personality = 'Lazy'
-          ORDER BY name`);
-
-    return lazyRes.rows;
-  }
-
-  /** Find all Normal villagers. */
-
-  static async findAllNormal() {
-    const normalRes = await db.query(
-      `SELECT id, name, image, personality
-          FROM villagers
-          WHERE personality = 'Normal'
-          ORDER BY name`);
-
-    return normalRes.rows;
-  }
-
-  /** Find all Peppy villagers. */
-
-  static async findAllPeppy() {
-    const peppyRes = await db.query(
-      `SELECT id, name, image, personality
-          FROM villagers
-          WHERE personality = 'Peppy'
-          ORDER BY name`);
-
-    return peppyRes.rows;
-  }
-
-  /** Find all Smug villagers. */
-
-  static async findAllSmug() {
-    const smugRes = await db.query(
-      `SELECT id, name, image, personality
-          FROM villagers
-          WHERE personality = 'Smug'
-          ORDER BY name`);
-
-    return smugRes.rows;
-  }
-
-  /** Find all Snooty villagers. */
-
-  static async findAllSnooty() {
-    const snootyRes = await db.query(
-      `SELECT id, name, image, personality
-          FROM villagers
-          WHERE personality = 'Snooty'
-          ORDER BY name`);
-
-    return snootyRes.rows;
-  }
-
-  /** Find all Uchi villagers. */
-
-  static async findAllUchi() {
-    const uchiRes = await db.query(
-      `SELECT id, name, image, personality
-          FROM villagers
-          WHERE personality = 'Uchi'
-          ORDER BY name`);
-
-    return uchiRes.rows;
-  }
-
   /** Given a villager name, return data about villager. */
 
   static async findOne(name) {
@@ -121,7 +24,7 @@ class Villager {
     return villager;
   }
 
-  /** Filter by personality and species */
+  /** Filter by personality and/or species */
 
   static async filter(personalities, species) {
     // need the ' in actual query
