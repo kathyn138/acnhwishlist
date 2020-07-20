92bf596c30abcce27183deb664b9e69127be0bf9
Kathy Nguyen <kathyn262@gmail.com>
2020-07-19 17:37:30 -0700

    added spacing for code readability

--- frontend/src/Wishlist.tsx
+++ frontend/src/Wishlist.tsx
@@ -24,6 +24,7 @@ class Wishlist extends React.PureComponent<WishlistProps> {
     return (
       <div className="col-md-5">
         <div className="wl text-center">
+          
           <div className="wl-data">
             <div className="wl-header">
               <h3>Wishlist</h3>
@@ -31,6 +32,7 @@ class Wishlist extends React.PureComponent<WishlistProps> {
             {this.props.wishlist.map(v =>
               <WishlistCard villager={v} removeFromWishlist={this.props.removeFromWishlist} />)}
           </div>
+
           <button type="button" className="btn wishlist-copy-btn"
             onClick={() => { navigator.clipboard.writeText(wishlistNames.join(', ')) }}>
             Copy to Clipboard</button>
