b1106e9e6f1802a2f1899f1e3fa621ff75b648bf
Kathy Nguyen <kathyn262@gmail.com>
2021-05-27 15:16:24 -0700

    added access control headers

--- backend/app.js
+++ backend/app.js
@@ -32,5 +32,12 @@ app.use(function (err, req, res, next) {
   });
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 module.exports = app;
