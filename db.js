757889fd358559739b90428dcf6ebf1d60657aa8
Kathy Nguyen <kathyn262@gmail.com>
2021-06-07 11:00:45 -0700

    removed extra client

--- db.js
+++ db.js
@@ -2,7 +2,7 @@
 
 const { Client } = require("pg");
 
-const client = new Client(process.env.DATABASE_URL || "postgresql:///villagerwishlist");
+// const client = new Client(process.env.DATABASE_URL || "postgresql:///villagerwishlist");
 
 let config;
 
