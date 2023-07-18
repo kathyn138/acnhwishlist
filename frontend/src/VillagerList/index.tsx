2abe1c89ea98d95ce59b39a7b1996895237a60db
Kathy Nguyen <kathyn262@gmail.com>
2023-07-17 21:09:46 -0700

    added loading spinner

--- frontend/src/VillagerList/index.tsx
+++ frontend/src/VillagerList/index.tsx
@@ -4,62 +4,73 @@ import VillagerCard from '../VillagerCard';
 
 type VillagerListProps = {
   villagers: {
-    id: string,
-    name: string,
-    image: string,
-    personality: string
-  }[],
+    id: string;
+    name: string;
+    image: string;
+    personality: string;
+  }[];
   wishlist: {
-    id: string,
-    name: string,
-    image: string,
-    personality: string
-  }[],
-  returnVillagersSearch: (newVillagers: {
-    id: string,
-    name: string,
-    image: string,
-    personality: string
-  }[]) => void;
+    id: string;
+    name: string;
+    image: string;
+    personality: string;
+  }[];
+  returnVillagersSearch: (
+    newVillagers: {
+      id: string;
+      name: string;
+      image: string;
+      personality: string;
+    }[]
+  ) => void;
   addToWishlist: (villager: {
-    id: string,
-    name: string,
-    image: string,
-    personality: string
-  }) => void,
+    id: string;
+    name: string;
+    image: string;
+    personality: string;
+  }) => void;
   removeFromWishlist: (villager: {
-    id: string,
-    name: string,
-    image: string,
-    personality: string
-  }) => void,
-}
+    id: string;
+    name: string;
+    image: string;
+    personality: string;
+  }) => void;
+};
 
 class VillagerList extends React.PureComponent<VillagerListProps> {
   render() {
-    let villagers = this.props.villagers.map(v =>
-      <VillagerCard villager={v} key={v.name}
+    let villagers = this.props.villagers.map((v) => (
+      <VillagerCard
+        villager={v}
+        key={v.name}
         wishlist={this.props.wishlist}
         addToWishlist={this.props.addToWishlist}
         removeFromWishlist={this.props.removeFromWishlist}
-      />);
+      />
+    ));
 
-    let loadingMessage = <React.Fragment>
-      <img className="loading-gif"
-        alt=""
-        src="https://cdn.discordapp.com/attachments/709643259789705317/732414740134887545/tenor.gif"></img>
-      <p className="loading-message">
-        Loading...
-      </p>
-    </React.Fragment>
+    let loadingMessage = (
+      <React.Fragment>
+        <img
+          className="loading-gif"
+          alt=""
+          src="https://cdn.discordapp.com/attachments/709643259789705317/732414740134887545/tenor.gif"
+        ></img>
+        <p className="loading-message">
+          <div className="spinner-border text-info" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        </p>
+      </React.Fragment>
+    );
 
-    let villagerListData = this.props.villagers.length ? villagers : loadingMessage
+    let villagerListData = this.props.villagers.length
+      ? villagers
+      : loadingMessage;
 
     return (
       <div className="vl">
-        <div className="vl-data">
-          {villagerListData}
-        </div>
+        <div className="vl-data">{villagerListData}</div>
       </div>
     );
   }
