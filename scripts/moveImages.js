dfd800346ae7e5927842160e6c06fffe8118811f
Kathy Nguyen <kathyn262@gmail.com>
2024-03-06 21:27:21 -0800

    capitalized folder names

--- scripts/moveImages.js
+++ scripts/moveImages.js
@@ -1,21 +1,46 @@
 const fs = require('fs');
 const path = require('path');
-const { crankyVillagers, jockVillagers, lazyVillagers,
-  normalVillagers, peppyVillagers, smugVillagers,
-  snootyVillagers, uchiVillagers } = require('./villagers');
+const {
+  crankyVillagers,
+  jockVillagers,
+  lazyVillagers,
+  normalVillagers,
+  peppyVillagers,
+  smugVillagers,
+  snootyVillagers,
+  uchiVillagers,
+} = require('./villagers');
 
-let personalityTypes = [crankyVillagers, jockVillagers, lazyVillagers,
-  normalVillagers, peppyVillagers, smugVillagers,
-  snootyVillagers, uchiVillagers];
+let personalityTypes = [
+  crankyVillagers,
+  jockVillagers,
+  lazyVillagers,
+  normalVillagers,
+  peppyVillagers,
+  smugVillagers,
+  snootyVillagers,
+  uchiVillagers,
+];
 
-let folderNames = ['cranky', 'jock', 'lazy', 'normal', 'peppy', 'smug',
-  'snooty', 'uchi'];
+let folderNames = [
+  'Cranky',
+  'Jock',
+  'Lazy',
+  'Normal',
+  'Peppy',
+  'Smug',
+  'Snooty',
+  'Uchi',
+];
 
 // create folders for personality types
 for (let name of folderNames) {
-  fs.mkdir(path.join(__dirname, `../frontend/src/Assets/Villagers/${name}`), (err) => {
-    if (err) throw err;
-  });
+  fs.mkdir(
+    path.join(__dirname, `../frontend/src/Assets/Villagers/${name}`),
+    (err) => {
+      if (err) throw err;
+    }
+  );
 }
 
 // move villager images into folders sorted by personality types
@@ -24,10 +49,19 @@ for (let i = 0; i < personalityTypes.length; i++) {
     let currVillager = personalityTypes[i][j];
     let currFolder = folderNames[i];
 
-    fs.rename(path.join(__dirname, `../frontend/src/Assets/Villagers/${currVillager}.png`),
-      path.join(__dirname,`../frontend/src/Assets/Villagers/${currFolder}/${currVillager}.png`), (err) => {
+    fs.rename(
+      path.join(
+        __dirname,
+        `../frontend/src/Assets/Villagers/${currVillager}.png`
+      ),
+      path.join(
+        __dirname,
+        `../frontend/src/Assets/Villagers/${currFolder}/${currVillager}.png`
+      ),
+      (err) => {
         if (err) throw err;
         console.log(`Move ${currVillager} complete!`);
-      });
+      }
+    );
   }
 }
