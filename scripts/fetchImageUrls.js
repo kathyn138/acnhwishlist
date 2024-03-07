d2105b4f83b1db5e559bd7bca3f58f2716676d93
Kathy Nguyen <kathyn262@gmail.com>
2024-03-06 23:26:44 -0800

    reformatted comments

--- scripts/fetchImageUrls.js
+++ scripts/fetchImageUrls.js
@@ -3,14 +3,16 @@ const util = require('util')
 const fs = require('fs');
 let content = ''
 
-// all images that we want have commanlities in their url: 
-// 1) https://oyster
-// 2) animal-crossing-new-horizons
-// 3) ends in .jpg, .gif, or .png
-
+/**
+ * all images that we want have commanlities in their url: 
+ * 1) https://oyster
+ * 2) animal-crossing-new-horizons
+ * 3) ends in .jpg, .gif, or .png
+ */
 axios.get('https://www.ign.com/wikis/animal-crossing-new-horizons/Villagers_and_Other_Characters').then(function(resp) {
   let pattern = /https:\/\/oyster.+?\/animal-crossing-new-horizons\/.+?\.(?:jpg|gif|png)/g;
   content = resp.data.match(pattern);
+  
   let unique = new Set(content);
 
   fs.writeFile('./imageUrls.txt', util.inspect(unique), "utf8", function(err) {
