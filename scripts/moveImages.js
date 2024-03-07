b3d4558a1629a6e4fa3294dbfa4167be80942ca4
Kathy Nguyen <kathyn262@gmail.com>
2024-03-06 21:25:09 -0800

    rewrote logic to move assets

--- scripts/moveImages.js
+++ scripts/moveImages.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 const { crankyVillagers, jockVillagers, lazyVillagers,
   normalVillagers, peppyVillagers, smugVillagers,
   snootyVillagers, uchiVillagers } = require('./villagers');
@@ -10,16 +11,21 @@ let personalityTypes = [crankyVillagers, jockVillagers, lazyVillagers,
 let folderNames = ['cranky', 'jock', 'lazy', 'normal', 'peppy', 'smug',
   'snooty', 'uchi'];
 
-// move villager images into folders
-// sorted by personality types 
+// create folders for personality types
+for (let name of folderNames) {
+  fs.mkdir(path.join(__dirname, `../frontend/src/Assets/Villagers/${name}`), (err) => {
+    if (err) throw err;
+  });
+}
 
+// move villager images into folders sorted by personality types
 for (let i = 0; i < personalityTypes.length; i++) {
   for (let j = 0; j < personalityTypes[i].length; j++) {
     let currVillager = personalityTypes[i][j];
     let currFolder = folderNames[i];
 
-    fs.rename(`./assets/villagers/${currVillager}.png`,
-      `./assets/villagers/${currFolder}/${currVillager}.png`, (err) => {
+    fs.rename(path.join(__dirname, `../frontend/src/Assets/Villagers/${currVillager}.png`),
+      path.join(__dirname,`../frontend/src/Assets/Villagers/${currFolder}/${currVillager}.png`), (err) => {
         if (err) throw err;
         console.log(`Move ${currVillager} complete!`);
       });
